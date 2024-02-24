1bb65217fa2be96ec2b67f166d7350163588b7e3
Ismail Can <caniscanmail@gmail.com>
2024-02-25 00:23:51 +0100

    student crud operations added

---
+++ TaskTracker/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Data;
+using TaskTracker.Models;
+
+namespace TaskTracker.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+             IEnumerable<Student> students = _db.Students.ToList();
+            return View(students);
+        }
+
+
+        public IActionResult Add()
+        {
+
+
+            return View();
+
+
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+           
+
+            _db.Students.Add(student);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+                   
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var  studentFromDb = _db.Students.FirstOrDefault(x => x.Id == id);
+
+            return View(studentFromDb);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student obj)
+        {
+            
+
+            _db.Students.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            var studentFromDb = _db.Students.FirstOrDefault(x => x.Id == id);
+
+            return View(studentFromDb);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Student obj)
+        {
+
+
+            _db.Students.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+
+    }
+}
